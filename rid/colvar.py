8f9fdc28ed090ae83b4497e6c421144275bd5773
yanze <yanze039@mit.edu>
2023-08-10 22:02:27 -0400

    update

--- rid/colvar.py
+++ rid/colvar.py
@@ -1,8 +1,8 @@
 import torch
 import numpy as np
-import MDAnalysis as mda
 from pathlib import Path
-from MDAnalysis.analysis.dihedrals import Dihedral
+# import MDAnalysis as mda
+# from MDAnalysis.analysis.dihedrals import Dihedral
 
 class DihedralAngle(torch.nn.Module):
 
@@ -36,33 +36,6 @@ class DihedralAngle(torch.nn.Module):
         # angle between v and w in a plane is the torsion angle
         x = (v*w).sum(-1)
         y = (torch.cross(v, w, dim=-1)* b1).sum(-1)
-        return torch.arctan2(y, x)
+        return torch.arctan2(y, x).requires_grad_(True)
 
-
-
-if __name__ == "__main__":
-    data = Path("../data/")
-    u = mda.Universe(data/"npt.gro")
-    dihedral_selection = []
-    for res in u.residues:
-        if res.resname == "SOL" or res.resname == "NA" or res.resname == "CL":
-            continue
-        if res.phi_selection() is None:
-            continue
-        else:
-            dihedral_selection.append([ii.index for ii in res.phi_selection()])
-    # help(u.residues[0])
-    ca = u.select_atoms('resid 1-10')
-    # print(ca.positions)
-    ags = [res.phi_selection() for res in u.residues[1:10]]
-    da = Dihedral(ags).run()
-    print("Dihedral angles from MDAnalysis:")
-    print(np.deg2rad(da.angles))
-    
-    dihedral_selection = np.array(dihedral_selection)
-    dih_positions = ca.positions[dihedral_selection.flatten()].reshape([-1, 4, 3])
-    mymodel = DihedralAngle()
-    dihedrals = mymodel(torch.from_numpy(dih_positions))
-    print("Dihedral angles from Model:")
-    print(dihedrals)
  
--- rid/model.py
+++ rid/model.py
@@ -2,37 +2,53 @@ import torch
 import torch.nn as nn
 import math
 from colvar import DihedralAngle
+# from common import prep_dihedral
+# import MDAnalysis as mda
+from pathlib import Path
+from typing import Dict, List, Tuple, Optional
+# from torch import vmap
 
-class MultiLinearLayer(nn.Module):
+
+class MLP(nn.Module):
     """ Custom Linear layer but mimics a standard linear layer """
     """ we can concatenate multiple linear layers to form a multi-linear layer """
-    def __init__(self, size_in, n_models, size_out):
+    def __init__(self, size_in, size_out, features=[64,64,64,64]):
         super().__init__()
-        self.size_in, self.size_out, self.n_models = size_in, size_out, n_models
-        weights = torch.Tensor(n_models, size_in, size_out)
-        self.weights = nn.Parameter(weights)  # nn.Parameter is a Tensor that's a module parameter.
-        bias = torch.Tensor(n_models, size_out)
-        self.bias = nn.Parameter(bias)
+        self.activation = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.1)
+        self.Sequence = nn.Sequential(
+            nn.Linear(size_in, features[0]),
+            self.activation,
+            self.dropout,
+            nn.Linear(features[0], features[1]),
+            self.activation,
+            self.dropout,
+            nn.Linear(features[1], features[2]),
+            self.activation,
+            self.dropout,
+            nn.Linear(features[2], features[3]),
+            self.activation,
+            self.dropout,
+            nn.Linear(features[3], size_out)
+        )
 
-        # initialize weights and biases
-        nn.init.kaiming_uniform_(self.weights, a=math.sqrt(5)) # weight init
-        fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weights)
-        bound = 1 / math.sqrt(fan_in)
-        nn.init.uniform_(self.bias, -bound, bound)  # bias init
-
-    def forward(self, x):
-        # x: [N_sample, N_CVs] --> [N_sample, 1, N_CVs, 1]
-        # self.weights: [N_models, N_CVs, N_features]
-        w_times_x= torch.matmul( x[..., None, None, :], self.weights).squeeze()  
-        # w_times_x: [N_sample, N_models, N_features]
-        # self.bias: [N_models, N_features] --> [1, N_models, N_features]
-        return torch.add(w_times_x, self.bias[None, ...])  # w times x + b
+    def forward(self, torsion):
+        # x: [N_sample, N_CVs] --> [N_sample, 1, N_CVs]
+        output = self.Sequence(torsion)
+        return output
 
 
-class FreeEnergyBias(nn.Module):
+
+
+class DihedralBias(nn.Module):
     """Free energy biasing potential."""
 
-    def __init__(self, colvar_fn, colvar_idx, kT, n_models, n_cvs, features=[64, 64, 64, 64]):
+    def __init__(self, 
+                 colvar_fn, colvar_idx, n_models, n_cvs, 
+                 features=[64, 64, 64, 64],
+                 e0=2,
+                 e1=3
+                 ):
         """Initialize the biasing potential.
 
             Parameters
@@ -47,25 +63,31 @@ class FreeEnergyBias(nn.Module):
                   The width of the biasing potential.
         """
         super().__init__()
-        self.colvar_fn = colvar_fn
-        self.kT = kT
-        self.colvar_idx = colvar_idx
+        self.colvar_fn = colvar_fn()
+        self.colvar_idx = torch.from_numpy(colvar_idx)
         self.n_models = n_models
+        self.n_cvs = n_cvs
 
+        self.layer_norm = nn.LayerNorm(2*n_cvs)
 
-        self.dense_list = [MultiLinearLayer(n_cvs, n_models, features[0])]
-        for ii in range(len(features)):
-            self.dense_list.append(MultiLinearLayer(features[ii], n_models, features[ii+1]))
-        self.dense_list.append(MultiLinearLayer(features[-1], n_models, n_cvs))
-        self.activation = nn.ReLU()
+        self.model1 = MLP(2 * n_cvs, 1, features=features)
+        self.model2 = MLP(2 * n_cvs, 1, features=features)
+        self.model3 = MLP(2 * n_cvs, 1, features=features)
+        self.model4 = MLP(2 * n_cvs, 1, features=features)
+        self.model_list = [self.model1, self.model2, self.model3, self.model4]
+        self.e0 = e0
+        self.e1 = e1
+        # just calculate once
+        self.e1_m_e0 = e1-e0
+        self.loss_fn = nn.MSELoss()
             
 
-    def forward(self, positions):
+    def forward(self, positions, boxvectors):
         """The forward method returns the energy computed from positions.
 
             Parameters
             ----------
-            positions : torch.Tensor with shape (nparticles,3)
+            positions : torch.Tensor with shape (nparticles, 3)
             positions[i,k] is the position (in nanometers) of spatial dimension k of particle i
 
             Returns
@@ -73,27 +95,65 @@ class FreeEnergyBias(nn.Module):
             potential : torch.Scalar
             The potential energy (in kJ/mol)
         """
-            
-        colvar = self.colvar_fn(positions[self.colvar_idx])
+        positions.requires_grad_(True)
+        boxsize = boxvectors.diag()
+        positions = positions - torch.floor(positions/boxsize)*boxsize  # remove PBC
+        selected_positions = positions[self.colvar_idx.flatten()].reshape([-1, 4, 3])  # [N_CVs, 4, 3]
+        # calculate CVs
+        cvs = self.colvar_fn(selected_positions)
+        energy, mean_sforces = self.get_energy_mean_force_from_torsion(cvs)
+        energy_ave = torch.mean(energy)
+        forces = torch.autograd.grad([energy_ave,], [positions,], allow_unused=True, create_graph=True, retain_graph=True)[0]
+        assert forces is not None
+        model_div = torch.mean(torch.var(mean_sforces, dim=-1)) ** 0.5
+        sigma = self.uncertainty_weight(model_div)
 
-        for layer in self.dense_list:
-            colvar = self.activation(layer(colvar))
-
-        return colvar
+        return (energy_ave * sigma, forces * sigma)
     
 
+    def uncertainty_weight(self, model_div):
+        iswitch = (self.e1-model_div)/self.e1_m_e0
+        # use heaviside function to make the gradient zero when iswitch is zero
+        uncertainty_weight = torch.heaviside(torch.div(iswitch, 1, rounding_mode='floor'), 0.5*(1+torch.cos(torch.pi*(1-iswitch))))
+        return uncertainty_weight
 
+    
+    def get_energy_from_torsion(self, torsion):
+        torsion = torch.cat([torch.cos(torsion), torch.sin(torsion)], dim=-1)
+        # torsion = self.layer_norm(torsion).reshape([-1, 2*self.n_cvs])
+        torsion = torsion.reshape([-1, 2*self.n_cvs])
+        # energy_list = torch.zeros(torsion.shape[0], self.n_models)
+        # energy_list = []
+        # for i, model in enumerate(self.model_list):
+        #     energy = model(torsion).reshape(-1)
+        #     # energy_list[:,i] = energy
+        #     energy_list.append( energy )
+        return torch.stack([
+                self.model1(torsion).reshape(-1),
+                self.model2(torsion).reshape(-1),
+                self.model3(torsion).reshape(-1),
+                self.model4(torsion).reshape(-1)
+            ], dim=-1)
+        # return torch.stack(energy_list, dim=-1)
+    
 
-# Render the compute graph to a TorchScript module
-# module = torch.jit.script(ForceModule())
+    def get_energy_mean_force_from_torsion(self, torsion):
+        model_energy = self.get_energy_from_torsion(torsion)
+        # print(model_energy)
+        force_list = []
+        grad_outputs : List[Optional[torch.Tensor]] = [ torch.ones_like(model_energy[:,0]) ]
+        for imodel in range(self.n_models):
+            mean_forces = torch.autograd.grad([model_energy[:,imodel],], [torsion,], grad_outputs=grad_outputs, retain_graph=True )[0]
+            assert mean_forces is not None
+            force_list.append(mean_forces)
+        force_list = torch.stack(force_list, dim=-1)
+        return model_energy, force_list
+    
 
-# Serialize the compute graph to a file
-# module.save('model.pt')
-
-if __name__ == "__main__":
-    mymodel = MultiLinearLayer(3, 2, 4)
-    mymodel2 = FreeEnergyBias(DihedralAngle(), [0,1,2,3], 1.0, 2, 4)
-    x = torch.randn((6,3))
-    mymodel(x)
-    # print(x)
-    # print(mymodel(x))
+    def mseloss(self, torsion):
+        energy_list = self.get_energy_from_torsion(self, torsion)
+        force_list = torch.zeros(energy_list.shape[0], self.n_models, self.n_cvs)
+        for imodel in range(self.n_models):
+            mean_forces = torch.autograd.grad(energy_list[:,imodel], torsion, retain_graph=True )[0]
+            force_list[imodel] = mean_forces
+        return energy_list, force_list
