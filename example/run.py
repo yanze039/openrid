eb07c12060d47f78632c66648417f1e896a4d1db
yanze <yanze039@mit.edu>
2023-09-29 17:22:41 -0400

    add argparser

--- example/run.py
+++ example/run.py
@@ -1,9 +1,16 @@
 import sys
+import argparse
 
 # add the path to the openrid package
 sys.path.append("..")
 from openrid import ReinforcedDynamics
 
+def parser_config():
+    parser = argparse.ArgumentParser(description="Run Reinforced Dynamics")
+    parser.add_argument("CONFIG", type=str, default="rid.yaml", help="path to the config file")
+    return parser.parse_args()
+
 if __name__ == "__main__":
-    rid = ReinforcedDynamics(config="./ala2.yaml")
+    args = parser_config()
+    rid = ReinforcedDynamics(config=args.CONFIG)
     rid.run()
